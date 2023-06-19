f4585f46dbbb4e8b42c93f300d99cc40048100c8
SanC005 <82254558+ReiZero1349@users.noreply.github.com>
2023-06-19 23:50:43 +0530

    added home

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Home from './components/home';
 import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Housing Horizon
-    </h1>
+    <Home/>
   );  
 }
 
---
+++ app/src/components/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './App.css';
+
+function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Housing Horizon
+    </h1>
+  );  
+}
+
+export default Home;
